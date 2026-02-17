170b6a52cf1ab7e8c6627b998f9aaa94a25974b5
Zane Helton <zane99@me.com>
2026-02-17 02:15:36 -0500

    Add test for logging invalid limit order

--- src/types.cc
+++ src/types.cc
@@ -3,28 +3,24 @@
 namespace order_book_v1 {
 std::ostream& operator<<(std::ostream& os, OrderSide const side) {
   switch (side) {
-    case OrderSide::kBuy: {
+    case OrderSide::kBuy:
       os << "BUY";
       break;
-    }
-    case OrderSide::kSell: {
+    case OrderSide::kSell:
       os << "SELL";
       break;
-    }
   }
   return os;
 }
 
 std::ostream& operator<<(std::ostream& os, TimeInForce const tif) {
   switch (tif) {
-    case TimeInForce::kGoodTillCancel: {
+    case TimeInForce::kGoodTillCancel:
       os << "GTC";
       break;
-    }
-    case TimeInForce::kImmediateOrCancel: {
+    case TimeInForce::kImmediateOrCancel:
       os << "IOC";
       break;
-    }
   }
   return os;
 }
